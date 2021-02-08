a3caf52689b98a93848b9458e5dc220dc1cf20d5
Shinrai <fuck@shinrai.cc>
2021-02-09 01:15:59 +0800

    Update baidu.js.sug.Qx.js

--- baidu.js.sug.Qx.js
+++ baidu.js.sug.Qx.js
@@ -1,7 +1,7 @@
 var body = $response.body;
 
 body = body.replace(
-    /bind\("click",function\(a,c,h\)\{return function\(e\)/,
-    'bind("click",function(a,c,h){return a&&a["length"]&&a[0]=="sug"?function(e){void 0}:function(e)');
+    /bind\(\\"click\\",function\(a,c,h\)\{return\s*?function\(e\)/,
+    'bind(\\"click\\",function(a,c,h){return a&&a["length"]&&a[0]=="sug"?function(e){void 0}:function(e)');
 
 $done(body);
