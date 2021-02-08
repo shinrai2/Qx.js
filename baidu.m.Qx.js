a11c54eab56c4559b27f9c0853f5fd9b956a1f16
shinrai2 <hello@shinrai.cc>
2021-02-08 13:02:26 +0800

    Update baidu.m.Qx.js

--- baidu.m.Qx.js
+++ baidu.m.Qx.js
@@ -3,5 +3,9 @@ var body = $response.body;
 body = body.replace(
     /id="logo"/,
     'id="logo" onclick="localStorage.clear();alert(\'localStorage clear done.\');"');
+body = body.replace(
+    /type="submit">/,
+    'type="submit">TEST'
+);
 console.log(body);
 $done(body);
