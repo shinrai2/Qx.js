5c0386e246642d80905fe0c6117ecb66e0b823e0
shinrai2 <hello@shinrai.cc>
2021-02-08 12:47:14 +0800

    CONSOLE

--- baidu.js.sug.Qx.js
+++ baidu.js.sug.Qx.js
@@ -3,5 +3,5 @@ var body = $response.body;
 body = body.replace(
     /bind\(\\"click\\",function\(a,c,h\)\{return function\(e\)/,
     'bind(\\"click\\",function(a,c,h){return a&&a["length"]&&a[0]=="sug"?function(e){void 0}:function(e)');
-
+console.log(body);
 $done(body);
--- baidu.m.Qx.js
+++ baidu.m.Qx.js
@@ -3,5 +3,5 @@ var body = $response.body;
 body = body.replace(
     /id="logo"/,
     'id="logo" onclick="localStorage.clear();alert(\'localStorage clear done.\');"');
-
+console.log(body);
 $done(body);
